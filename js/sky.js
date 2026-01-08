26335f76ce27cd9e9f29d1ece049bd013f4a0faa
Brayn1822 <brayanfelipe1807@gmail.com>
2026-01-08 18:30:44 -0500

    Delete js directory

--- js/sky.js
+++
@@ -1,8 +0,0 @@
-for (let i = 0; i < 120; i++) {
-  const star = document.createElement("div");
-  star.className = "star";
-  star.style.left = Math.random() * 100 + "vw";
-  star.style.top = Math.random() * 70 + "vh";
-  star.style.animationDelay = Math.random() * 3 + "s";
-  document.body.appendChild(star);
-}
